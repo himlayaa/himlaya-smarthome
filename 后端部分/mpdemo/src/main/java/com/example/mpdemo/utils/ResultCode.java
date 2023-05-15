088e77b096193560ae821a68f8eb679b9f50a29d
himlaya <firemaple17@gmail.com>
2023-05-15 13:55:26 +0800

    delete a file

--- 后端部分/mpdemo/src/main/java/com/example/mpdemo/utils/ResultCode.java
+++
@@ -1,7 +0,0 @@
-package com.example.mpdemo.utils;
-
-public interface ResultCode {
-    public static Integer SUCCESS = 20000;
-    public static Integer ERROR = 20001;
-    public static Integer WARN = 10000;
-}
